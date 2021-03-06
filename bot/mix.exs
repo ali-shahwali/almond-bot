6ddb50c82c37a08ad81c3de2790415607dd875a6
Ali Shahwali <ali_zinq@hotmail.com>
2021-03-06 08:07:25 +0100

    removed old dependencies

--- bot/mix.exs
+++ bot/mix.exs
@@ -21,15 +21,10 @@ defmodule Bot.MixProject do
 
   defp deps do
     [
-      #{:poison, "~> 4.0.1", override: true},
-      {:google_api_you_tube, "~> 0.33"},
       {:jason, "~> 1.2"},
       {:httpotion, "~> 3.1.3"},
-      {:tesla, "~> 1.4.0"},
       {:porcelain, "~> 2.0"},
-      {:alchemy, "~> 0.6.8", hex: :discord_alchemy},
-      {:floki, "~> 0.30.0"},
-      #{:crawly, "~> 0.13.0"},
+      {:alchemy, "~> 0.6.8", hex: :discord_alchemy}
     ]
   end
 
